626be8da270542739a13d468fa99a17c7466c33d
Mohammad Umar Khan <Khanumar620@gmail.com>
2020-08-05 22:07:53 +0300

    new update

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
-import pyautogui
-pyautogui.moveTo(760,300)
-pyautogui.typewrite("HELLO I AM UMAR THE CONQUERER I MSHSBLHLN FELFHEF N;S FOFOHEOH AN",interval=0.5)
+a = "hello"
+b = "world"
+print(a+b)
